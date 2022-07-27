70d2dc9ea04e2f6f6687060bce5271ce5928d24a
Thith-thith <thiththin762@gmail.com>
2022-07-27 10:49:41 +0700

    updated faucet ui

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Header from './components/Header';
 import Testnet from './pages/testnet';
@@ -11,22 +6,25 @@ import Testnet from './pages/testnet';
 function App() {
   return (
     <BrowserRouter>
-      <Toaster 
-        position="top-right"
+      <Toaster
+        position="bottom-right"
+        reverseOrder={true}
         toastOptions={{
           style: {
+            width: '450px',
+            height: '180px',
             color: '#FFF',
-            backgroundColor: 'rgba(255, 255, 255, .1)',
+            backgroundColor: 'rgba(255, 255, 255, .3)',
             backdropFilter: 'blur(10px)',
             border: '1px solid hsla(0, 0%, 65%, 0.158)',
-            padding: '8px 16px'
-          }
+            padding: '8px 16px',
+          },
         }}
       />
       <Header />
       <Routes>
-        <Route path='/' element={<Navigate to='/testnet' />} />
-        <Route path='/testnet' element={<Testnet />} />
+        <Route path="/" element={<Navigate to="/testnet" />} />
+        <Route path="/testnet" element={<Testnet />} />
       </Routes>
     </BrowserRouter>
   );
